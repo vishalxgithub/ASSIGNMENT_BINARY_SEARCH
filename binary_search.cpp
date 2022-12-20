b3c13ebb75417078001930432084e725badc8d60
Vishal Gupta <114002191+vishalxgithub@users.noreply.github.com>
2022-12-20 21:09:50 +0530

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        int mid = s+(e-s)/2;
+        while(s <= e){
+            if(nums[mid] == target)
+                return mid;
+            if(target < nums[mid])
+                e = mid -1;
+            else
+                s = mid+1;
+            mid = s+(e-s)/2;
+        }
+        return -1;
+    }
+};
